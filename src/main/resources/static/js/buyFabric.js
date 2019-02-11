31df827f4abe1910324431a78cd0865ca53b9b77
Anian2009 <Anian2009@ukr.net>
2019-02-11 16:29:21 +0200

    Some change in pom.xml

--- src/main/resources/static/js/buyFabric.js
+++ src/main/resources/static/js/buyFabric.js
@@ -16,15 +16,38 @@ $(document).ready(function () {
         }
     });
 
+    let getUsersInfo = function () {
+        $.ajax('/api/user/dashboard', {
+            type: 'GET',
+            dataType: 'json',
+            contentType: 'application/json',
+            headers: {token: sessionStorage.getItem("token")},
+            success: function (data) {
+                model.userInfo = data.user;
+                model.usersInfo = data.users;
+                initialize();
+            },
+            error: function (jqXHR) {
+                alert(jqXHR.responseJSON.status +" - "+jqXHR.responseJSON.message);
+            },
+        });
+    };
+
     var Vm = function (model) {
         this.model = model;
         var self = this;
 
+        this.name = ko.observable('');
+        this.silverBalance = ko.observable('');
+        this.goldBalance = ko.observable('');
+        this.increase = ko.observable('');
         this.fabricName = ko.observable('');
         this.price = ko.observable('');
         this.miningPerSecond = ko.observable('');
         this.upgrade = ko.observable('');
         this.fabrics = ko.observableArray();
+        this.userInfo = ko.observableArray();
+        this.usersInfo = ko.observableArray();
 
         this.buyFabric = function(vieModel){
             self.model.buyFabric(vieModel);
@@ -32,11 +55,15 @@ $(document).ready(function () {
 
         initialize = function () {
             self.fabrics(self.model.fabrics);
+            self.userInfo(self.model.userInfo);
+            self.usersInfo(self.model.usersInfo);
         }
     };
 
     var Model = function () {
         this.fabrics = [];
+        this.userInfo = [];
+        this.usersInfo = [];
 
         this.buyFabric = function (vieModel) {
             $.ajax('/api/user/buy-factory', {
@@ -57,4 +84,15 @@ $(document).ready(function () {
 
     var model = new Model();
     ko.applyBindings(new Vm(model));
+
+    $('#logOut').click(function () {
+        sessionStorage.clear();
+        window.location = '../index.html';
+    });
+
+    setInterval(function () {
+        getUsersInfo();
+    }, 1000);
+
+    getUsersInfo();
 });
